61ece26adda8830c22393710faa82bccff6489f5
mmichaelb <michael.bruns@protonmail.com>
2020-11-12 17:06:01 +0100

    add return statements after shutdown calls and add exit statement to shutdown function

--- pkg/analyser.go
+++ pkg/analyser.go
@@ -4,12 +4,12 @@ import (
 	"github.com/multiplay/go-ts3"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 	"log"
+	"os"
 )
 
 type Analyser struct {
 	config                    *Config
 	teamSpeakClient           *ts3.Client
-	teamSpeakReadStopChan     chan struct{}
 	teamSpeakNotificationChan chan ts3.Notification
 	neo4jDriver               neo4j.Driver
 	neo4jSession              neo4j.Session
@@ -24,16 +24,19 @@ func (analyser *Analyser) Connect() {
 	if err := analyser.connectNeo4j(); err != nil {
 		log.Printf("Could not connect to Neo4j database server: %v", err)
 		analyser.Shutdown()
+		return
 	}
 	log.Println("Connected to Neo4j database server!")
 	log.Println("Connecting to TeamSpeak server...")
 	if err := analyser.connectTeamSpeak(); err != nil {
 		log.Printf("Could not connect to TeamSpeak server: %v", err)
 		analyser.Shutdown()
+		return
 	}
 	if err := analyser.setupTeamSpeak(); err != nil {
 		log.Printf("Could not setup TeamSpeak server: %v", err)
 		analyser.Shutdown()
+		return
 	}
 	log.Println("Connected to TeamSpeak server!")
 }
@@ -42,4 +45,5 @@ func (analyser *Analyser) Shutdown() {
 	log.Println("Shutting down analyser...")
 	analyser.closeTeamSpeak()
 	analyser.closeNeo4j()
+	os.Exit(0)
 }
